319c4dc34aa1c0d12fc7e977100518387e91bb67
IdrisSahnoun <idrissahnoun@gmail.com>
2024-11-22 12:07:16 +0100

    started_workshop_2_added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,23 @@ import { ResidencesComponent } from './components/residences/residences.componen
 import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ResidenceDetailsComponentComponent } from './components/residences/residence-details-component/residence-details-component.component';
+import { AddResidenceComponentComponent } from './components/residences/add-residence-component/add-residence-component.component';
+import { ApartmentsComponentComponent } from './components/Appartments/apartments-component/apartments-component.component';
+import { ApartmentsByResidenceComponentComponent } from './components/Appartments/apartments-by-residence-component/apartments-by-residence-component.component';
+import { AddApartmentComponentComponent } from './components/Appartments/add-apartment-component/add-apartment-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ResidencesComponent,
     HeaderComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    ResidenceDetailsComponentComponent,
+    AddResidenceComponentComponent,
+    ApartmentsComponentComponent,
+    ApartmentsByResidenceComponentComponent,
+    AddApartmentComponentComponent
   ],
   imports: [
     BrowserModule,
